9eb26ebb96836ca3407dfba63905a3899c1db40c
raythurman2386 <raymondthurman5@gmail.com>
2019-12-03 22:26:58 -0500

    get users correctly working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 let db = require("./data/db");
 const server = express();
+const host = "127.0.0.1";
 const port = 5000;
 
 server.use(express.json());
@@ -14,7 +15,11 @@ server.get("/", (req, res) => {
 
 // GET request to /api/users
 // Returns array of all user objects contained
-server.get("/api/users", (req, res) => {});
+server.get("/api/users", (req, res) => {
+  db.find().then(data => {
+    res.status(200).json(data);
+  });
+});
 
 // GET request to /api/users/:id
 // returns the user with specified id
@@ -32,6 +37,6 @@ server.delete("/api/users/:id", (req, res) => {});
 // updates the specified user with info from request body
 server.put("/api/users:id", (req, res) => {});
 
-server.listen(port, () => {
-  console.log(`Server running on http://locahost:${port}`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
