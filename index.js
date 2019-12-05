66f520ca32ccf7023d9c320f5cadfdc155b8a2d9
raythurman2386 <raymondthurman5@gmail.com>
2019-12-04 19:29:10 -0500

    fixed post request

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ server.post("/api/users", (req, res) => {
     name: req.body.name,
     bio: req.body.bio,
     created_at: Date.now(),
-    updated_at: null
+    updated_at: Date.now()
   };
 
   db.insert(newUser).then(user => {
@@ -75,7 +75,25 @@ server.delete("/api/users/:id", (req, res) => {
 });
 
 // PUT request to /api/users/:id
-server.put("/api/users:id", (req, res) => {});
+// server.put("/api/users:id", (req, res) => {
+//   if (!req.body.name || !req.body.bio) {
+//     return res
+//       .status(400)
+//       .json({ message: "Please provide a name and a bio for the user" });
+//   }
+
+//   db.update(req.params.id, req.body)
+//     .then(user => {
+//       return user !== 0
+//         ? res.status(200).json({ ...req.body })
+//         : res.status(404).json({ message: "That user does not exist!" });
+//     })
+//     .catch(() => {
+//       return res
+//         .status(500)
+//         .json({ error: "The user information could not be modified." });
+//     });
+// });
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
