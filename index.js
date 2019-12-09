29c6d638363a67f103b6b4ff8b4d1dff388dd508
raythurman2386 <raymondthurman5@gmail.com>
2019-12-08 20:17:40 -0500

    refactored routes to add in catch

--- index.js
+++ index.js
@@ -11,28 +11,39 @@ server.use(cors());
 
 // GET request to /api/users
 server.get("/api/users", (req, res) => {
-  db.find().then(data => {
-    if (!data) {
-      res.status(500).json({
-        errorMessage: "There was an error while saving the user to the database"
-      });
-    } else {
+  db.find()
+    .then(data => {
+      if (!data) {
+        return res.status(404).json({
+          errorMessage: "There are no users in the database"
+        });
+      }
+
       res.status(200).json(data);
-    }
-  });
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ error: "The user information could not be accessed." });
+    });
 });
 
 // GET request to /api/users/:id
 server.get("/api/users/:id", (req, res) => {
-  db.findById(req.params.id).then(user => {
-    if (!user) {
-      res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
-    } else {
+  db.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
       res.status(200).json(user);
-    }
-  });
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
 });
 
 // POST request to /api/users
@@ -50,24 +61,35 @@ server.post("/api/users", (req, res) => {
     updated_at: Date.now()
   };
 
-  db.insert(newUser).then(user => {
-    res
-      .status(200)
-      .json({ user, addedUser: newUser, message: "You added a user!" });
-  });
+  db.insert(newUser)
+    .then(user => {
+      res
+        .status(200)
+        .json({ addedUser: newUser, message: "You added a user!" });
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ error: "The user information could not be added." });
+    });
 });
 
 // DELETE request to /api/users/:id
 server.delete("/api/users/:id", (req, res) => {
-  db.findById(req.params.id).then(user => {
-    if (!user) {
-      res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
-    } else {
+  db.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
       db.remove(req.params.id).then(i => res.status(200).json(user));
-    }
-  });
+    })
+    .catch(() => {
+      return res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
 });
 
 // PUT request to /api/users/:id
