0546d00dd5f1c3a2b6c05fc06d43c1a3d702b8a8
raythurman2386 <raymondthurman5@gmail.com>
2020-01-12 01:54:32 -0500

    removed user routes

--- api/routes/routes.index.js
+++ api/routes/routes.index.js
@@ -1,7 +1,5 @@
 const testRoutes = require("./testRoutes");
-const userRoutes = require("./userRoutes");
 
 module.exports = server => {
   server.use("/", testRoutes);
-  server.use("/api/users", userRoutes);
 };
