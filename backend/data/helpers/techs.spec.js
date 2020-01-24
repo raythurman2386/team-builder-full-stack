421599bf0e042f68f2254556756b197c56f09c90
raythurman2386 <raymondthurman5@gmail.com>
2020-01-23 20:03:51 -0500

    backend fully tested

--- backend/data/helpers/techs.spec.js
+++ backend/data/helpers/techs.spec.js
@@ -6,7 +6,23 @@ beforeEach(async () => {
 })
 
 describe("techs models", () => {
-  test("testing", async () => {
-    expect(2 + 2).toBe(4)
+  test("get techs", async () => {
+    const res = await techsModel.getTechs()
+    expect(res.length).toBe(6)
+  })
+
+  test("get tech by id", async () => {
+    const res = await techsModel.getTechById(1)
+    expect(res.name).toMatch(/herb/i)
+  })
+
+  test("get tech jobs", async () => {
+    const res = await techsModel.getTechnicianJobs(1)
+    expect(res.length).toBeGreaterThan(0)
+  })
+
+  test("add tech", async () => {
+    const res = await techsModel.insert({ name: "test" })
+    expect(res[0]).toBe(7)
   })
 })
