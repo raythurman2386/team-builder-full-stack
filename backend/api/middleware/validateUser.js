bafb0b435e4b975639726c8a14af67366af8f662
raythurman2386 <raymondthurman5@gmail.com>
2020-01-23 20:39:41 -0500

    refactored validateUser

--- backend/api/middleware/validateUser.js
+++ backend/api/middleware/validateUser.js
@@ -9,22 +9,6 @@ const validateUser = () => {
         .json({ message: "Please provide a name and a bio for the user" });
     }
 
-    let date;
-
-    if (!req.body.created_at) {
-      date = new Date().toISOString();
-    } else {
-      date = req.body.created_at;
-    }
-
-    const updatedUser = {
-      name: req.body.name,
-      bio: req.body.bio,
-      created_at: date,
-      updated_at: new Date().toISOString()
-    };
-
-    req.user = updatedUser;
     next();
   };
 };
