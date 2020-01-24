1a2008341c90994eaf579cbf47ac7418dce96ff6
raythurman2386 <raymondthurman5@gmail.com>
2020-01-23 19:37:55 -0500

    tech routes tested

--- backend/api/server.spec.js
+++ backend/api/server.spec.js
@@ -79,4 +79,40 @@ describe("job routes", () => {
   })
 })
 
-describe("tech routes", () => { })
+describe("tech routes", () => {
+  test("get techs route", async () => {
+    const res = await supertest(server).get('/api/technicians')
+
+    expect(res.status).toBe(200);
+    expect(res.type).toBe("application/json")
+    expect(res.body.length).toBeGreaterThan(0)
+  })
+
+  test("get tech by id", async () => {
+    const res = await supertest(server).get('/api/technicians/1')
+
+    expect(res.status).toBe(200);
+    expect(res.type).toBe("application/json")
+    expect(res.body.name).toMatch(/herb/i)
+  })
+
+  test("get tech jobs", async () => {
+    const res = await supertest(server).get('/api/technicians/1/jobs')
+
+    expect(res.status).toBe(200);
+    expect(res.type).toBe("application/json")
+    expect(res.body.length).toBeGreaterThan(0)
+  })
+
+  test("add tech", async () => {
+    const tech = { name: "test" }
+    const res = await supertest(server)
+      .post('/api/technicians')
+      .send(tech)
+
+
+    expect(res.status).toBe(200);
+    expect(res.type).toBe("application/json")
+    expect(res.body[0]).toBe(7)
+  })
+})
