04a8ac89c00b77253de6b0f7afdd54433436b4d8
raythurman2386 <raymondthurman5@gmail.com>
2020-01-23 19:53:57 -0500

    job models tested

--- backend/data/helpers/jobs.spec.js
+++ backend/data/helpers/jobs.spec.js
@@ -6,7 +6,41 @@ beforeEach(async () => {
 })
 
 describe("jobs models", () => {
-  test("testing", () => {
-    expect(2 + 2).toBe(4)
+  test("get jobs", async () => {
+    const res = await jobsModel.getJobs()
+    expect(res.length).toBe(15)
+  })
+
+  test("get job by id", async () => {
+    const res = await jobsModel.getJobById(1)
+    expect(res.machine).toBe("259d")
+  })
+
+  test("add job", async () => {
+    const job = {
+      machine: "test",
+      complaint: "test",
+      tech_id: 1
+    }
+
+    const res = await jobsModel.addJob(job)
+    expect(res.id).toBe(18)
+  })
+
+  test("update job", async () => {
+    const job = {
+      id: 1,
+      machine: "test",
+      complaint: "test",
+      tech_id: 1
+    }
+
+    const res = await jobsModel.updateJob(1, job)
+    expect(res).toBe(1)
+  })
+
+  test("delete job", async () => {
+    const res = await jobsModel.deleteJob(1)
+    expect(res.id).toBe(1)
   })
 })
