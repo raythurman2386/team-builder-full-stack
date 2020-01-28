afc44fe7c0ca9c494fc1ebb168c779a6dd9cb90d
raythurman2386 <raymondthurman5@gmail.com>
2020-01-28 12:06:12 -0500

    fixed test routes

--- backend/api/routes/routes.index.js
+++ backend/api/routes/routes.index.js
@@ -3,7 +3,7 @@ const techRouter = require('./techRoutes');
 const jobsRouter = require('./jobRouter');
 
 module.exports = server => {
-  server.use("/", testRoutes);
   server.use("/api/technicians", techRouter);
   server.use('/api/jobs', jobsRouter);
+  server.use("/", testRoutes);
 };
