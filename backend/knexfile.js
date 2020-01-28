3cbc3cf2d0f9487a3565c18f7ac5662230b29b17
raythurman2386 <raymondthurman5@gmail.com>
2020-01-28 12:05:44 -0500

    returned dev and test to sqlite

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,24 +1,15 @@
 require('dotenv').config();
 const pg = require('pg');
 
-const localPg = {
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS
-};
+pg.defaults.ssl = true;
 
-if (process.env.NODE_ENV === 'production') {
-  pg.defaults.ssl = true;
-}
-
-const dbConnection = process.env.DATABASE_URL || localPg;
+const dbConnection = process.env.DATABASE_URL;
 // Update with your config settings.
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: localPg,
+    client: 'sqlite3',
+    connection: { filename: './data/team.db3' },
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
@@ -27,12 +18,14 @@ module.exports = {
       directory: './data/seeds'
     },
     pool: {
-      min: 2, max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
   },
   test: {
-    client: 'pg',
-    connection: localPg,
+    client: 'sqlite3',
+    connection: { filename: './data/test.db3' },
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
@@ -41,8 +34,10 @@ module.exports = {
       directory: './data/seeds'
     },
     pool: {
-      min: 2, max: 10
-    },
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    }
   },
 
   production: {
@@ -56,4 +51,4 @@ module.exports = {
       directory: './data/seeds'
     },
   },
-};
+}
