7756ab59204fa8d409c35765316fca7f72dbfcf9
raythurman2386 <raymondthurman5@gmail.com>
2020-02-07 18:56:43 -0500

    knexfile refactor and corrections

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
-require("dotenv").config();
-const server = require("./api/server");
+require('dotenv').config()
+const server = require('./api/server')
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
+
+server.use((req, res, next) => {
+  return res.status(404).json({ message: 'That route does not exist' })
+})
 
 server.use((err, req, res, next) => {
   console.log(err)
-  res.status(500).json(err)
+  return res.status(500).json(err)
 })
 
 server.listen(port, () => {
-  console.log(`The server is listening on http://localhost:${port}`);
-});
+  console.log(`The server is listening on http://localhost:${port}`)
+})
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,43 +1,34 @@
-require('dotenv').config();
-const pg = require('pg');
+require('dotenv').config()
+const pg = require('pg')
 
-pg.defaults.ssl = true;
+process.env.DATABASE_URL ? (pg.defaults.ssl = true) : (pg.defaults.ssl = false)
 
-const dbConnection = process.env.DATABASE_URL;
-// Update with your config settings.
+const dbConnection = process.env.DATABASE_URL || localPg
+
+const sqlite3 = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    }
+  }
+}
 
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './data/team.db3' },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    ...sqlite3,
+    connection: { filename: './data/team.db3' }
   },
   test: {
-    client: 'sqlite3',
-    connection: { filename: './data/test.db3' },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    }
+    ...sqlite3,
+    connection: { filename: './data/test.db3' }
   },
 
   production: {
@@ -49,6 +40,6 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
-    },
-  },
+    }
+  }
 }
