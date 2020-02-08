1a699fca3c55c1cc7a23795dbc0700c880f13434
raythurman2386 <raymondthurman5@gmail.com>
2020-02-08 13:29:45 -0500

    rendering from the api

--- frontend/src/components/Dashboard/listItems.jsx
+++ frontend/src/components/Dashboard/listItems.jsx
@@ -1,40 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
 import ListItem from "@material-ui/core/ListItem"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import ListItemText from "@material-ui/core/ListItemText"
 import PersonIcon from "@material-ui/icons/Person"
+import { TechContext } from "../../context/context"
 
-export const mainListItems = (
-  <div>
-    <ListItem button align='center'>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary='Herb' />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary='Devon' />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary='Jason' />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary='Josh' />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PersonIcon />
-      </ListItemIcon>
-      <ListItemText primary='Noah' />
-    </ListItem>
-  </div>
-)
+const MainListItems = () => {
+  const { techs } = useContext(TechContext)
+
+  return (
+    <div>
+      {techs &&
+        techs.map(tech => (
+          <ListItem key={tech.id} button align='center'>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary={tech.name} />
+          </ListItem>
+        ))}
+    </div>
+  )
+}
+
+export default MainListItems
