25ec7c20a2665dd80dde63e23fcc4af5404f5f0b
raythurman2386 <raymondthurman5@gmail.com>
2020-02-07 21:54:50 -0500

    all tech routes working properly

--- backend/router/techRoutes.js
+++ backend/router/techRoutes.js
@@ -1,10 +1,10 @@
 const techRouter = require('express').Router()
-const Techs = require('../models/Model')
+const { Tech } = require('../models/Model')
 
 techRouter
   .get('/', async (req, res, next) => {
     try {
-      const techs = await Techs.find()
+      const techs = await Tech.find()
       return res.status(200).json(techs)
     } catch (error) {
       console.log(error)
@@ -13,24 +13,24 @@ techRouter
   })
   .get('/:id', async (req, res, next) => {
     try {
-      const tech = await Techs.findBy({ id: req.params.id })
+      const tech = await Tech.findBy({ id: req.params.id })
       return res.status(200).json(tech)
     } catch (error) {
       console.log(error)
       next(error)
     }
   })
-  // .get('/:id/jobs', async (req, res, next) => {
-  //   try {
-  //     const jobs = await Techs.getTechnicianJobs(req.params.id)
-  //     return res.status(200).json(jobs);
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // })
+  .get('/:id/jobs', async (req, res, next) => {
+    try {
+      const jobs = await Tech.findTechJobs(req.params.id)
+      return res.status(200).json(jobs)
+    } catch (error) {
+      next(error)
+    }
+  })
   .post('/', async (req, res, next) => {
     try {
-      const newTech = await Techs.add(req.body)
+      const newTech = await Tech.add(req.body)
       return res.status(200).json(newTech)
     } catch (error) {
       next(error)
