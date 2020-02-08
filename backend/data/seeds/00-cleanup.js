b2fba9a4b781d793c799c2dbdb9b472ced38b71a
raythurman2386 <raymondthurman5@gmail.com>
2020-02-07 20:03:18 -0500

    seed files created

---
+++ backend/data/seeds/00-cleanup.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const cleaner = require('knex-cleaner')
+
+exports.seed = function(knex) {
+  return cleaner.clean(knex, {
+    mode: 'truncate',
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+  })
+}
