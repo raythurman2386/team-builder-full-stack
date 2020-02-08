1c6cbcb492b090eaaa2923f8995d99ee35e3cec6
raythurman2386 <raymondthurman5@gmail.com>
2020-02-08 01:15:04 -0500

    creating material ui frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { Switch, Route } from "react-router-dom"
+import SignInSide from "./components/SignIn/SignIn"
+import Register from "./components/Register/Register"
+import Dashboard from "./components/Dashboard/Dashboard"
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Switch>
+        <Route exact path="/" component={<h1>Hello</h1>} />
+        <Route exact path="/login" component={SignInSide} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </div>
+  )
+}
+
+export default App
