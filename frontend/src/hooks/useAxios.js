4de1ba2c3bbbb4b1db09e7c7eb52a814629749cc
raythurman2386 <raymondthurman5@gmail.com>
2020-02-09 19:51:34 -0500

    refactored axios call

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react"
 import { axiosWithAuth as axios } from "../utils/axiosConfig"
 
-export const useAxios = url => {
-  const [value, setValue] = useState()
+export const useAxios = (name, url) => {
+  const item = JSON.parse(localStorage.getItem(name))
+  const [value, setValue] = useState(item)
 
   useEffect(() => {
     axios()
       .get(url)
-      .then(res => setValue(res.data))
+      .then(res => {
+        setValue(res.data)
+        localStorage.setItem(name, JSON.stringify(res.data))
+      })
       .catch(err => console.log(err.response))
-  }, [url])
+  }, [name, url])
 
   return [value, setValue]
 }
