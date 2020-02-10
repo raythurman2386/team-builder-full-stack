d8e92204ec4e32626b4ddfcd844d27510613fdf0
raythurman2386 <raymondthurman5@gmail.com>
2020-02-09 21:04:01 -0500

    basic layout for add job

--- frontend/src/components/Modal/AddJob.jsx
+++ frontend/src/components/Modal/AddJob.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
 import IconButton from "@material-ui/core/IconButton"
 import Typography from "@material-ui/core/Typography"
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline"
@@ -21,8 +23,12 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function AddTech({ open, handleOpen }) {
+function AddJob({ open, handleOpen }) {
   const classes = useStyles()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
 
   return (
     <div>
@@ -33,11 +39,70 @@ export default function AddTech({ open, handleOpen }) {
         <AddCircleOutlineIcon color='secondary' />
       </IconButton>
       <SpringModal open={open} handleOpen={handleOpen}>
-        <div className={classes.paper}>
-          <h2 id='add job'>Add Job</h2>
-          <p>react-spring animates me.</p>
-        </div>
+        <form className={classes.paper} handleSubmit={handleSubmit}>
+          <h2 id='spring-modal-title'>Add Job</h2>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='machine'
+            label='Machine'
+            name='machine'
+            autoComplete='machine'
+            autoFocus
+            // value={name}
+            // onChange={e => setName(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='complaint'
+            label='Complaint'
+            name='complaint'
+            autoComplete='complaint'
+            // value={name}
+            // onChange={e => setName(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='serial_number'
+            label='Serial Number'
+            name='serial_number'
+            autoComplete='serial number'
+            // value={name}
+            // onChange={e => setName(e.target.value)}
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='tech_name'
+            label='Technician Name'
+            name='tech_name'
+            autoComplete='name'
+            // value={name}
+            // onChange={e => setName(e.target.value)}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='secondary'
+            className={classes.submit}
+          >
+            Add Job
+          </Button>
+        </form>
       </SpringModal>
     </div>
   )
 }
+
+export default AddJob
