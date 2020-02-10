acf9101e661332b147f0b52f372ab5d0d8407aad
raythurman2386 <raymondthurman5@gmail.com>
2020-02-09 19:51:46 -0500

    added setTechs to tech context

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -3,14 +3,17 @@ import { useAxios } from "../hooks/useAxios"
 import { TechProvider, JobProvider, MessageProvider } from "../context/context"
 
 const Context = ({ children }) => {
-  const [techs] = useAxios("http://localhost:4000/api/technicians")
-  const [jobs, setJobs] = useAxios("http://localhost:4000/api/jobs")
+  const [techs, setTechs] = useAxios(
+    "techs",
+    "http://localhost:4000/api/technicians"
+  )
+  const [jobs, setJobs] = useAxios("jobs", "http://localhost:4000/api/jobs")
   const [message, setMessage] = React.useState(
     localStorage.getItem("message") || ""
   )
 
   return (
-    <TechProvider value={{ techs }}>
+    <TechProvider value={{ techs, setTechs }}>
       <JobProvider value={{ jobs, setJobs }}>
         <MessageProvider value={{ message, setMessage }}>
           {children}
