3c95b57679a49f3a4c370f1748879c279e16987f
raythurman2386 <raymondthurman5@gmail.com>
2020-02-09 19:52:22 -0500

    refactored list items to use add tech modal

--- frontend/src/components/Dashboard/listItems.jsx
+++ frontend/src/components/Dashboard/listItems.jsx
@@ -3,14 +3,13 @@ import ListItem from "@material-ui/core/ListItem"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import ListItemText from "@material-ui/core/ListItemText"
 import PersonIcon from "@material-ui/icons/Person"
-import AddIcon from "@material-ui/icons/Add"
 import { TechContext } from "../../context/context"
-import SpringModal from "../../utils/SpringModal"
 import useToggle from "../../hooks/useToggle"
+import AddTech from "../Modal/AddTech"
 
 const MainListItems = () => {
   const { techs } = useContext(TechContext)
-  const [toggle, handleToggle] = useToggle()
+  const [open, handleOpen] = useToggle()
 
   return (
     <div>
@@ -23,14 +22,7 @@ const MainListItems = () => {
             <ListItemText primary={tech.name} />
           </ListItem>
         ))}
-      <SpringModal open={toggle} handleOpen={handleToggle}>
-        <ListItem button align='center' onClick={handleToggle}>
-          <ListItemIcon>
-            <AddIcon />
-          </ListItemIcon>
-          <ListItemText primary='New Tech' />
-        </ListItem>
-      </SpringModal>
+      <AddTech open={open} handleOpen={handleOpen} />
     </div>
   )
 }
