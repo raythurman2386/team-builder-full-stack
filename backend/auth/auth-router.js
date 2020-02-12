7bee37e53efaef6307c3318c7336210ac2aeaff2
raythurman2386 <raymondthurman5@gmail.com>
2020-02-11 21:44:45 -0500

    finalish auth router refactor

--- backend/auth/auth-router.js
+++ backend/auth/auth-router.js
@@ -5,37 +5,21 @@ const sendgrid = require("../utils/sendgrid")
 
 authRouter
   .post('/register', validateRegister(), hashPassword(), (req, res, next) => {
-    try {
-      return res
-        .status(201)
-        .json({ message: 'You have been successfully registered' })
-    } catch (error) {
-      next(error)
-    }
+    return res
+      .status(201)
+      .json({ message: 'You have been successfully registered' })
   })
-  .post('/login', validateLogin(), verifyPassword(), async (req, res, next) => {
-    try {
-      return res.status(200).json({
-        message: `Welcome ${req.name}`,
-        token: req.token
-      })
-    } catch (error) {
-      next(error)
-    }
+  .post('/login', validateLogin(), verifyPassword(), (req, res, next) => {
+    return res.status(200).json({
+      message: `Welcome ${req.name}`,
+      token: req.token
+    })
   })
   .post('/initial-reset', sendgrid, (req, res, next) => {
-    try {
-      return res.status(200).json({ message: 'Check your email' })
-    } catch (error) {
-      next(error)
-    }
+    return res.status(200).json({ message: 'Check your email' })
   })
-  .post('/reset-password', async (req, res, next) => {
-    try {
-      return res.status(200).json({ message: 'Password successfully updated' })
-    } catch (error) {
-      next(error)
-    }
+  .post('/reset-password', (req, res, next) => {
+    return res.status(200).json({ message: 'Password successfully updated' })
   })
 
 module.exports = authRouter
