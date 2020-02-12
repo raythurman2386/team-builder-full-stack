c1866519342f1316067112c0a19b32e5e990dbcc
raythurman2386 <raymondthurman5@gmail.com>
2020-02-11 21:05:25 -0500

    created sendgrid function

---
+++ backend/utils/sendgrid.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail')
+
+function sendgrid() {
+  return (req, res, next) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+    const msg = {
+      to: req.body.email,
+      from: 'jeffshop@helpdesk.com',
+      subject: 'Reset Password',
+      text: 'Click below to reset your password',
+      html:
+        '<a href="https://team-builder-pg.herokuapp.com/api/auth/reset-password">Reset Password</a>'
+    }
+    sgMail.send(msg)
+
+    next()
+  }
+}
+
+module.exports = sendgrid
