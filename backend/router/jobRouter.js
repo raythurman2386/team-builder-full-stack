ec0220c6693b8ddfe7f3fe36e80f9afbd83a1a5f
raythurman2386 <raymondthurman5@gmail.com>
2020-02-13 22:11:11 -0500

    middleware implemented

--- backend/router/jobRouter.js
+++ backend/router/jobRouter.js
@@ -1,5 +1,6 @@
 const jobsRouter = require('express').Router()
 const { validateJobs, validateJobId, validateDelete } = require('../middleware/jobs/validateJobs')
+const { validateJobInputs, validateUpdate } = require("../middleware/jobs/jobForms")
 
 jobsRouter
   .get('/', validateJobs(), (req, res) => {
@@ -8,39 +9,11 @@ jobsRouter
   .get('/:id', validateJobId(), (req, res) => {
     res.json(req.job)
   })
-  .post('/', async (req, res, next) => {
-    try {
-      const { tech_name } = req.body;
-      const tech = await Tech.findBy({ name: tech_name })
-      const job = {
-        machine: req.body.machine,
-        complaint: req.body.complaint,
-        serial_number: req.body.serial_number,
-        tech_id: tech.id,
-        created_by: req.userId
-      }
-      const id = await Job.add(job)
-      return res.status(201).json(id)
-    } catch (error) {
-      next(error)
-    }
+  .post('/', validateJobInputs(), (req, res) => {
+    res.status(201).json(req.newJob)
   })
-  .put('/:id', validateJobId(), async (req, res, next) => {
-    try {
-      const { tech_name } = req.body;
-      const tech = await Tech.findBy({ name: tech_name })
-      const job = {
-        machine: req.body.machine,
-        complaint: req.body.complaint,
-        serial_number: req.body.serial_number,
-        tech_id: tech.id,
-        created_by: req.userId
-      }
-      const updated = await Job.update(req.params.id, job)
-      return res.status(200).json(req.body)
-    } catch (error) {
-      next(error)
-    }
+  .put('/:id', validateJobId(), validateUpdate(), (req, res, next) => {
+    res.json({ message: "Job updated successfully" })
   })
   .delete('/:id', validateJobId(), validateDelete(), (req, res) => {
     res.json({ message: "Job deleted successfully" })
