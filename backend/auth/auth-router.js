d788b8f23a5bbf8cfc79fc17da35eefe7364ba5d
raythurman2386 <raymondthurman5@gmail.com>
2020-02-13 21:52:05 -0500

    validateJobs and Id

--- backend/auth/auth-router.js
+++ backend/auth/auth-router.js
@@ -5,21 +5,21 @@ const { handleReset } = require("../middleware/auth/validateReset")
 const sendgrid = require("../utils/sendgrid")
 
 authRouter
-  .post('/register', validateRegister(), hashPassword(), (req, res, next) => {
+  .post('/register', validateRegister(), hashPassword(), (req, res) => {
     res
       .status(201)
       .json({ message: 'You have been successfully registered' })
   })
-  .post('/login', validateLogin(), verifyPassword(), (req, res, next) => {
+  .post('/login', validateLogin(), verifyPassword(), (req, res) => {
     res.json({
       message: `Welcome ${req.name}`,
       token: req.token
     })
   })
-  .post('/initial-reset', sendgrid, (req, res, next) => {
+  .post('/initial-reset', sendgrid, (req, res) => {
     res.json({ message: 'Check your email' })
   })
-  .post('/reset-password', handleReset(), (req, res, next) => {
+  .post('/reset-password', handleReset(), (req, res) => {
     res.json({ message: 'Password successfully updated' })
   })
 
---
+++ backend/middleware/jobs/validateJobs.js
@@ -0,0 +1,33 @@
+const { Job, Tech } = require('../models/Model')
+
+function validateJobs() {
+  return async (req, res, next) => {
+    try {
+      const jobs = await Job.find()
+      req.jobs = jobs
+
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+function validateJobId() {
+  return async (req, res, next) => {
+    try {
+      const job = await Job.findBy({ id: req.params.id })
+
+      if (!job) {
+        return res.status(404).json({ message: "Job not found" })
+      }
+
+      req.job = job
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+module.exports = { validateJobs, validateJobId }
