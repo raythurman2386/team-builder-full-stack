5e17810fb8e9d927e686275c097a133adbad55b5
raythurman2386 <raymondthurman5@gmail.com>
2020-02-16 20:01:55 -0500

    added useInput custom hook

---
+++ frontend/src/hooks/useInput.js
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export const useInput = () => {
+  const [value, setValue] = useState('')
+
+  const handleChange = newValue => {
+    setValue(newValue)
+  }
+
+  return [value, handleChange]
+}
