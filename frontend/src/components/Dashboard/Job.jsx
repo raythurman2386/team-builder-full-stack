ac7216a1c7e6e014bdf300092642bc49523b97b4
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-27 18:22:27 -0400

    Feat: created singular job component

---
+++ frontend/src/components/Dashboard/Job.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import { useMutation } from '@apollo/react-hooks';
+import { GET_JOBS, DELETE_JOB } from '../../queries';
+
+const Job = ({ job, props }) => {
+  const classes = useStyles();
+  const [deleteJob] = useMutation(DELETE_JOB, {
+    update(cache, { data: { deleteJob } }) {
+      const { jobs } = cache.readQuery({ query: GET_JOBS });
+      cache.writeQuery({
+        query: GET_JOBS,
+        data: { jobs: jobs.filter(job => job.id !== deleteJob.id) }
+      });
+    }
+  });
+
+  return (
+    <TableRow
+      key={job.id}
+      className={classes.row}
+      onClick={() => props.history.push(`/dashboard/job/${job.id}`)}
+    >
+      <TableCell align='center'>{job.machine}</TableCell>
+      <TableCell align='center'>{job.complaint}</TableCell>
+      {job.tech ? (
+        <TableCell align='center'>{job.tech.name}</TableCell>
+      ) : (
+        <TableCell align='center'>Needs Tech</TableCell>
+      )}
+      <TableCell
+        align='center'
+        className={classes.delete}
+        onClick={e => deleteJob({ variables: { id: job.id } })}
+      >
+        <DeleteForeverOutlinedIcon color='secondary' />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default Job;
+
+const useStyles = makeStyles(theme => ({
+  delete: {
+    cursor: 'pointer'
+  },
+  row: {
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: 'rgb(7, 177, 77, 0.42)'
+    }
+  }
+}));
--- frontend/src/components/Dashboard/Jobs.jsx
+++ frontend/src/components/Dashboard/Jobs.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 import Title from './Title';
+import Job from './Job';
 
 // Apollo Deps
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import { GET_JOBS, DELETE_JOB } from '../../queries';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_JOBS } from '../../queries';
 
 function Jobs({ props }) {
   const { loading, error, data } = useQuery(GET_JOBS);
-  const [deleteJob] = useMutation(DELETE_JOB, {
-    update(cache, { data: { deleteJob } }) {
-      const { jobs } = cache.readQuery({ query: GET_JOBS });
-      cache.writeQuery({
-        query: GET_JOBS,
-        data: { jobs: jobs.filter(job => job.id !== deleteJob.id) }
-      });
-    }
-  });
-
-  const classes = useStyles();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -43,28 +31,7 @@ function Jobs({ props }) {
         </TableHead>
         <TableBody>
           {data.jobs &&
-            data.jobs.map(job => (
-              <TableRow
-                key={job.id}
-                className={classes.row}
-                onClick={() => props.history.push(`/job/${job.id}`)}
-              >
-                <TableCell align='center'>{job.machine}</TableCell>
-                <TableCell align='center'>{job.complaint}</TableCell>
-                {job.tech ? (
-                  <TableCell align='center'>{job.tech.name}</TableCell>
-                ) : (
-                  <TableCell align='center'>Needs Tech</TableCell>
-                )}
-                <TableCell
-                  align='center'
-                  className={classes.delete}
-                  onClick={e => deleteJob({ variables: { id: job.id } })}
-                >
-                  <DeleteForeverOutlinedIcon color='secondary' />
-                </TableCell>
-              </TableRow>
-            ))}
+            data.jobs.map(job => <Job key={job.id} job={job} props={props} />)}
         </TableBody>
       </Table>
     </React.Fragment>
@@ -72,15 +39,3 @@ function Jobs({ props }) {
 }
 
 export default Jobs;
-
-const useStyles = makeStyles(theme => ({
-  delete: {
-    cursor: 'pointer'
-  },
-  row: {
-    cursor: 'pointer',
-    '&:hover': {
-      backgroundColor: 'rgb(7, 177, 77, 0.42)'
-    }
-  }
-}));
