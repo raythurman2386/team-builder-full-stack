ce2599c3e54768fb995f9d65a95575d05ebc79f3
Raymond Thurman <raymondthurman5@gmail.com>
2020-03-27 18:22:39 -0400

    Feat: Initial update job implementation

--- frontend/src/components/Modal/UpdateJob.jsx
+++ frontend/src/components/Modal/UpdateJob.jsx
@@ -2,18 +2,15 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import SpringModal from '../../utils/SpringModal';
 import { useMutation, useQuery } from '@apollo/react-hooks';
 import { UPDATE_JOB, GET_JOBS, GET_TECHS } from '../../queries';
+import Layout from '../Layout/Layout';
 
-function UpdateJob(props, { open, handleOpen }) {
+function UpdateJob({ props }) {
   const classes = useStyles();
   const { loading, error, data } = useQuery(GET_TECHS);
   const [updateJob] = useMutation(UPDATE_JOB, {
@@ -28,9 +25,9 @@ function UpdateJob(props, { open, handleOpen }) {
   });
 
   const [job, setJob] = useState({
-    machine: props.job.machine,
-    complaint: props.job.complaint,
-    tech_id: props.job.tech_id
+    machine: '',
+    complaint: '',
+    tech_id: 0
   });
 
   const handleChange = e => {
@@ -46,102 +43,88 @@ function UpdateJob(props, { open, handleOpen }) {
         tech_id: job.tech_id
       }
     })
-      .then(res => handleOpen())
+      .then(res => console.log(res))
       .catch(err => alert(err.message));
   };
 
   return (
-    <div>
-      <IconButton color='inherit' onClick={handleOpen}>
-        <Typography className={classes.title} color='inherit' noWrap>
-          Add Job
-        </Typography>
-        <AddCircleOutlineIcon color='secondary' />
-      </IconButton>
-      <SpringModal open={open} handleOpen={handleOpen}>
-        <form className={classes.paper} onSubmit={handleSubmit}>
-          <h2 id='spring-modal-title'>Add Job</h2>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='machine'
-            label='Machine'
-            name='machine'
-            autoComplete='machine'
-            autoFocus
-            value={job.machine}
+    <Layout>
+      <form className={classes.paper} onSubmit={handleSubmit} fullWidth>
+        <h2 id='spring-modal-title'>Update Job</h2>
+        <TextField
+          variant='outlined'
+          margin='normal'
+          required
+          fullWidth
+          id='machine'
+          label='Machine'
+          name='machine'
+          autoComplete='machine'
+          autoFocus
+          value={job.machine}
+          onChange={e => handleChange(e)}
+        />
+        <TextField
+          variant='outlined'
+          margin='normal'
+          required
+          fullWidth
+          id='complaint'
+          label='Complaint'
+          name='complaint'
+          autoComplete='complaint'
+          value={job.complaint}
+          onChange={e => handleChange(e)}
+        />
+        <FormControl
+          fullWidth
+          variant='outlined'
+          className={classes.formControl}
+        >
+          <InputLabel id='techs'>Tech</InputLabel>
+          <Select
+            labelId='techs'
+            value={job.tech_id}
+            name='tech_id'
             onChange={e => handleChange(e)}
-          />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='complaint'
-            label='Complaint'
-            name='complaint'
-            autoComplete='complaint'
-            value={job.complaint}
-            onChange={e => handleChange(e)}
-          />
-          <FormControl
-            fullWidth
-            variant='outlined'
-            className={classes.formControl}
           >
-            <InputLabel id='techs'>Tech</InputLabel>
-            <Select
-              labelId='techs'
-              value={job.tech_id}
-              name='tech_id'
-              onChange={e => handleChange(e)}
-            >
-              {loading && (
-                <MenuItem value={job.tech_id}>Loading . . .</MenuItem>
-              )}
-              {error && (
-                <MenuItem value={job.tech_id}>There's been an error</MenuItem>
-              )}
-              <MenuItem value={job.tech_id}>Select a Tech</MenuItem>
-              {data &&
-                data.techs.map(tech => (
-                  <MenuItem key={tech.id} value={tech.id}>
-                    {tech.name}
-                  </MenuItem>
-                ))}
-            </Select>
-          </FormControl>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='secondary'
-            className={classes.submit}
-          >
-            Update Job
-          </Button>
-        </form>
-      </SpringModal>
-    </div>
+            {loading && <MenuItem value={job.tech_id}>Loading . . .</MenuItem>}
+            {error && (
+              <MenuItem value={job.tech_id}>There's been an error</MenuItem>
+            )}
+            <MenuItem value={job.tech_id}>Select a Tech</MenuItem>
+            {data &&
+              data.techs.map(tech => (
+                <MenuItem key={tech.id} value={tech.id}>
+                  {tech.name}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          color='secondary'
+          className={classes.submit}
+        >
+          Update Job
+        </Button>
+      </form>
+    </Layout>
   );
 }
 
 export default UpdateJob;
 
 const useStyles = makeStyles(theme => ({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
   paper: {
     backgroundColor: theme.palette.background.paper,
     border: 'none',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    width: '800px',
+    width: '100%',
+    margin: '0 auto',
     height: '600px'
   },
   formControl: {
