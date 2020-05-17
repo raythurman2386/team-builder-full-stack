94147ec43f28b700c4555cf1e922d8cc22ff16ad
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-17 10:19:49 -0400

    still troubleshooting cors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { BrowserRouter as Router, withRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import App from './App';
 
 const client = new ApolloClient({
-  uri: "https://graphql-teambuilder.herokuapp.com/graphql",
+  uri: 'https://graphql-teambuilder.herokuapp.com/graphql'
 });
 
 const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-  <Router>
-    <ApolloProvider client={client}>
+  <ApolloProvider client={client}>
+    <Router>
       <AppWithRouter />
-    </ApolloProvider>
-  </Router>,
-  document.getElementById("root"),
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root')
 );
