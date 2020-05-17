f966cee8a102b4f3f2a00e00454e1806aebc8cf2
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-16 21:37:57 -0400

    reverted uri change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,11 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
 import App from "./App";
 
-const AppWithRouter = withRouter(App);
-
 const client = new ApolloClient({
-  uri: "https://graphql-teambuilder.herokuapp.com",
+  uri: "https://graphql-teambuilder.herokuapp.com/graphql",
 });
+
+const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
   <Router>
